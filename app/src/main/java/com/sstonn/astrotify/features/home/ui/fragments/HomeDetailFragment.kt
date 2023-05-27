7c4845a7a9f52eb9923a5bfbf8f370d31792dfbb
toan.pns <toan.pns@amanotes.com>
2023-05-27 13:28:56 +0700

    [NAVIGATION] [Implement navigation main]

--- app/src/main/java/com/sstonn/astrotify/features/home/ui/fragments/HomeDetailFragment.kt
+++ app/src/main/java/com/sstonn/astrotify/features/home/ui/fragments/HomeDetailFragment.kt
@@ -5,30 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedDispatcher
 import androidx.navigation.fragment.findNavController
 import com.sstonn.astrotify.R
-import com.sstonn.astrotify.databinding.FragmentHomeBinding
+import com.sstonn.astrotify.databinding.FragmentHomeDetailBinding
 
-class HomeFragment : Fragment() {
-    lateinit var binding: FragmentHomeBinding
+class HomeDetailFragment : Fragment() {
+    private lateinit var binding: FragmentHomeDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding = FragmentHomeDetailBinding.inflate(layoutInflater)
+
         // Inflate the layout for this fragment
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.gotoHomeDetailBtn.setOnClickListener {
-            val action = HomeFragmentDirections.actionHomeFragmentToHomeDetailFragment()
-            findNavController().navigate(action)
+        findNavController().setOnBackPressedDispatcher(dispatcher = OnBackPressedDispatcher{
 
-        }
+        })
     }
-
-
 }
